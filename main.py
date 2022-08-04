168c930a29ebe4790fe4d8458a8b6fa968920cec
Vincentiime <claireaux.vincent@gmail.com>
2022-08-04 17:21:47 +0200

    adding command line not working

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+
 if __name__ == '__main__':
+    import click
+
+    @click.command()
+
     from wqp.workflow import model_training_workflow
     model_training_workflow(data_path='http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv')
